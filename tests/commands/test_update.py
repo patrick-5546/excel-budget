0ad3e0905917d548815ecad22c1b466fdd1d620d
Patrick Creighton <pcreighton429@gmail.com>
2023-03-20 03:59:43 -0700

    Skeleton code for update

---
+++ tests/commands/test_update.py
@@ -0,0 +1,25 @@
+import argparse
+import inspect
+
+import pytest
+
+from excelbudget.commands.update import Update
+from excelbudget.configure import Configuration
+
+
+def test_config_args_static() -> None:
+    """Check if `config_args` is static
+    as this is not guaranteed by the current implementation.
+    """
+    assert isinstance(inspect.getattr_static(Update, "configure_args"), staticmethod)
+
+
+def test_update() -> None:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    Update.configure_args(subparsers)
+    args = parser.parse_args(["update", "."])
+
+    config = Configuration(args=args)
+    with pytest.raises(NotImplementedError):
+        Update(config)
