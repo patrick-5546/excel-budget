71bf2c87d5a1b568fc37aeab63114d325292244a
Patrick Creighton <pcreighton429@gmail.com>
2023-03-22 02:31:47 -0700

    Implement the generate command (#6)
    * Add openpyxl dependency
    
    * Write __init__
    
    * Add openpyxl to dictionary
    
    * Add excel extension
    
    * Add types-openxl dependency
    
    * Write create_year_sheet
    
    * Remove Command function docstrings
    
    * Fix type hints
    
    * Check if file exists
    
    * Add logging
    
    * Update PR template

--- src/xlbudget/commands.py
+++ src/xlbudget/commands.py
@@ -1,52 +1,48 @@
 """The commands, implemented as implementations of the abstract class `Command`."""
 
+import datetime
+import os
 import sys
 from abc import ABC, abstractmethod
 from argparse import ArgumentParser, Namespace, _SubParsersAction
 from logging import getLogger
 from typing import List, Type
 
+from openpyxl import Workbook
+
+from .xlfunctions import create_year_sheet
+
 logger = getLogger(__name__)
 
 
 class Command(ABC):
-    """The abstract class that the command implementations implement."""
+    """The abstract class that the command implementations implement.
+
+    Attributes:
+        path (str): The path to the xlbudget file.
+    """
 
     @property
     @abstractmethod
     def name(self) -> str:
-        """The command's CLI name. Should be implemented as a class attribute"""
         pass
 
     @property
     @abstractmethod
     def aliases(self) -> List[str]:
-        """The command's CLI aliases. Should be implemented as a class attribute"""
         pass
 
     @classmethod
     @abstractmethod
     def configure_args(cls, subparsers: _SubParsersAction) -> None:
-        """Adds the command's CLI arguments to the argument parser.
-
-        Args:
-            subparsers (_SubParsersAction): The command `subparsers`.
-        """
         pass
 
     @abstractmethod
     def __init__(self, args: Namespace) -> None:
-        """Initializes the command instance, storing the relevant CLI arguments as
-        instance variables.
-
-        Args:
-            args (Namespace): The CLI arguments.
-        """
-        pass
+        self.path = args.path
 
     @abstractmethod
     def run(self) -> None:
-        """Runs the command."""
         pass
 
 
@@ -56,6 +52,9 @@ class Generate(Command):
     Attributes: Class Attributes
         name (str): The command's CLI name.
         aliases (List[str]): The command's CLI aliases.
+
+    Attributes:
+        force (bool): If True and file exists, will overwrite it.
     """
 
     name: str = "generate"
@@ -81,10 +80,32 @@ class Generate(Command):
         )
 
     def __init__(self, args: Namespace) -> None:
-        pass
+        super().__init__(args)
+        self.force = args.force
+
+        logger.debug(f"instance variables: {vars(self)}")
 
     def run(self) -> None:
-        raise NotImplementedError
+        """Creates an empty xlbudget file populated with:
+
+        - A sheet for the current year.
+
+        Raises:
+            FileExistsError: If `self.force` is false and the file exists.
+        """
+        logger.info("generating an empty xlbudget file")
+
+        if not self.force and os.path.exists(self.path):
+            raise FileExistsError(
+                f"File {self.path} exists, run with -f/--force to overwrite"
+            )
+
+        wb = Workbook()
+        year = str(datetime.date.today().year)
+        logger.info(f"creating {year} sheet")
+        create_year_sheet(wb, year)
+        logger.info(f"saving to {self.path}")
+        wb.save(self.path)
 
 
 class Update(Command):
