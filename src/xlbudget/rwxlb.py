d236bf57dfc76c7b0b3f9660189e3448df74316b
Patrick Creighton <pcreighton429@gmail.com>
2023-03-27 14:55:54 -0700

    Make col headers and formats constants

--- src/xlbudget/rwxlb.py
+++ src/xlbudget/rwxlb.py
@@ -14,6 +14,9 @@ FORMAT_DATE = "MM/DD/YYYY"
 
 MONTH_NAME_0_IND = calendar.month_name[1:]
 
+COL_NAMES = ["Date", "Description", "Amount"]
+COL_FORMATS = [FORMAT_DATE, None, FORMAT_ACCOUNTING]
+
 
 def create_year_sheet(wb: Workbook, year: str):
     """Creates a year sheet, with a table for each month.
@@ -25,12 +28,8 @@ def create_year_sheet(wb: Workbook, year: str):
     ws = wb.create_sheet(year)
     num_tables = len(MONTH_NAME_0_IND)
 
-    table_headers = ["Date", "Description", "Amount"]
-    table_formats = [FORMAT_DATE, None, FORMAT_ACCOUNTING]
-    for c_start in range(
-        1, (len(table_headers) + 1) * num_tables + 1, len(table_headers) + 1
-    ):
-        month_ind = c_start // (len(table_headers) + 1)
+    for c_start in range(1, (len(COL_NAMES) + 1) * num_tables + 1, len(COL_NAMES) + 1):
+        month_ind = c_start // (len(COL_NAMES) + 1)
         month = MONTH_NAME_0_IND[month_ind]
         logger.debug(f"creating {month} table")
 
@@ -40,30 +39,30 @@ def create_year_sheet(wb: Workbook, year: str):
             start_row=1,
             start_column=c_start,
             end_row=1,
-            end_column=c_start + len(table_headers) - 2,
+            end_column=c_start + len(COL_NAMES) - 2,
         )
 
         # table sum
-        sum = ws.cell(row=1, column=c_start + len(table_headers) - 1)
-        sum.value = f"=SUM({month}[{table_headers[-1]}])"
+        sum = ws.cell(row=1, column=c_start + len(COL_NAMES) - 1)
+        sum.value = f"=SUM({month}[{COL_NAMES[-1]}])"
         sum.number_format = FORMAT_ACCOUNTING
         logger.debug(f"created sum cell {sum.coordinate}='{sum.value}'")
 
         # table header and formating
-        for i in range(len(table_headers)):
+        for i in range(len(COL_NAMES)):
             c = c_start + i
 
             # header
-            ws.cell(row=2, column=c).value = table_headers[i]
+            ws.cell(row=2, column=c).value = COL_NAMES[i]
 
             # formatting
             cell = ws.cell(row=3, column=c)
-            if table_formats[i]:
-                cell.number_format = table_formats[i]
+            if COL_FORMATS[i]:
+                cell.number_format = COL_FORMATS[i]
 
         # create table
         c_start_ltr = get_column_letter(c_start)
-        c_end_ltr = get_column_letter(c_start + len(table_headers) - 1)
+        c_end_ltr = get_column_letter(c_start + len(COL_NAMES) - 1)
         ref = f"{c_start_ltr}2:{c_end_ltr}3"
         logger.debug(f"table {ref=}")
         tab = Table(displayName=month, ref=ref)
