5aa4d459961701579b1ed46f0048493365e17eaa
Patrick Creighton <pcreighton429@gmail.com>
2023-05-10 23:08:56 +0800

    Add comments to existing command tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -12,13 +12,14 @@ AVAILABLE_COMMANDS = {
 
 
 @pytest.mark.parametrize("cmd_cls", AVAILABLE_COMMANDS)
-def test_command_config_args_is_classmethod(cmd_cls: Type[commands.Command]) -> None:
+def test_config_args(cmd_cls: Type[commands.Command]) -> None:
+    # each command class should implement configure_args() as a classmethod
     assert isinstance(inspect.getattr_static(cmd_cls, "configure_args"), classmethod)
 
 
 def test_get_command_classes() -> None:
+    # the output of get_command_classes() should be the same as `AVAILABLE_COMMANDS`
     command_classes = commands.get_command_classes()
-
     assert len(command_classes) == len(AVAILABLE_COMMANDS)
     for cmd in command_classes:
         assert cmd in AVAILABLE_COMMANDS
